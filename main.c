d9a24cc60908813957286957de88c1eeade50daf
yeangal <56105092+yeangal@users.noreply.github.com>
2022-03-10 12:39:40 -0800

    Update CountFiles(), Bug fixes

--- main.c
+++ main.c
@@ -5,21 +5,43 @@
 #include <string.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 
-int fileCounter = 0;
+int fileCounter;
 pthread_mutex_t fileLock;
 struct dirent *dirEntry;
 
-
-    //Lock Mutex
-    //Stat() file
-    //Unlock Mutex
-    //Exit thread
 void *copyFiles() {
-    struct stat buffer;
-    while(pthread_mutex_lock(&fileLock) != 0) {
+    char backupFile;
+    FILE *source, *destination;
 
-    }
+    printf("dirEntry = %s\n", dirEntry->d_name);
+    // strcat(backupFile, ".backup/");
+    // source = fopen(dirEntry, "r");
+    // if(source == NULL) {
+    //     fprintf(stderr, "Failed to open source file: %s\n", strerror(errno));
+    //     exit(1);
+    // }
+    // destination = fopen(".backup/", "w");
+    // if(destination == NULL) {
+    //     fprintf(stderr, "Failed to create backup file: %s\n", strerror(errno));
+    //     exit(1);
+    // }
+
+    // if(pthread_mutex_lock(&fileLock) != 0) {
+    //     fprintf(stderr, "Failed to lock mutex: %s\n", strerror(errno));
+    //     exit(1);
+    // }
+
+    // while(1) {
+
+    // }
+
+    // if(pthread_mutex_unlock(&fileLock) != 0) {
+    //     fprintf(stderr, "Failed to unlock mutex: %s\n", strerror(errno));
+    //     exit(1);
+    // }
+    // pthread_exit(NULL);
     
 }
 
@@ -39,24 +61,51 @@ void threadHandler() {
     pthread_mutex_destroy(&fileLock);
 }
 
+/*
+Opens current directory and iterates through each file
+If file is a regular file, call threadHandler() and increment fileCounter
+If file is a sub directory, recursively call countFiles()
+*/
 int countFiles() {
-    DIR *dir = opendir("./");
+    char cwd[256];
+    DIR *dir;
 
-    if(dir == NULL) {
-        fprintf(stderr, "Failed to open directory ./: %s\n", strerror(errno));
+    if(getcwd(cwd, sizeof(cwd)) == NULL) {
+        fprintf(stderr, "Failed to get current working directory: %s\n", strerror(errno));
         return 1;
     }
 
+    dir = opendir(cwd);
+    printf("cwd: %s\n", cwd);
+    if(dir == NULL) {
+        fprintf(stderr, "Failed to open directory: %s\n", strerror(errno));
+        return 1;
+    }
+
+    printf("Check 1\n");
+    sleep(2);
     if(dirEntry == NULL) {
         dirEntry = readdir(dir);
+        if(dirEntry == NULL) {
+            fprintf(stderr, "Failed to read directory: %s\n", strerror(errno));
+            return 1;
+        }
     }
-    
-    while(dirEntry != NULL) {
+
+    printf("Check 2\n");
+    sleep(2);
+    while((dirEntry = readdir(dir)) != NULL) {
+        printf("Check 3\n");
+        sleep(2);
         if(dirEntry->d_type == DT_REG) {
+            printf("Check 4: %s\n", dirEntry->d_name);
+            sleep(2);
             threadHandler();
             fileCounter++;
         }
         else if(dirEntry->d_type == DT_DIR) {
+            printf("Check 5: %s\n", dirEntry->d_name);
+            sleep(2);
             countFiles();
         }
     }
@@ -92,7 +141,9 @@ int directoryHandler() {
 }
 
 int main(int argc, char *argv[]) {
-    
+    fileCounter = 0;
+    dirEntry = NULL;
+
     //Check for -r command
     if(argc > 1) {
         //Call function to restore all backup files from .backup/
@@ -100,6 +151,9 @@ int main(int argc, char *argv[]) {
 
     if(directoryHandler() == 1) {
         exit(1);
+    }
+    else {
+        countFiles();
     }
     
     return 0;
