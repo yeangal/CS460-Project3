8b95a2e8af911ab458c336958d0df1d200752c9a
yeangal <56105092+yeangal@users.noreply.github.com>
2022-03-10 23:14:37 -0800

    Fixed args to copyFiles(). Bug Fixes

--- main.c
+++ main.c
@@ -7,7 +7,13 @@
 #include <pthread.h>
 #include <unistd.h>
 
+struct node {
+    char *filename;
+    char *filepath;
+};
+
 int fileCounter;
+struct node *newNode;
 struct dirent *dirEntry;
 pthread_mutex_t fileLock;
 
@@ -15,34 +21,38 @@ void *copyFiles() {
     char backupFile[256];
     FILE *source, *destination;
 
-    // printf("dirEntry = %s\n", dirEntry->d_name);
-    // strcat(backupFile, ".backup/");
-    // source = fopen(dirEntry, "r");
-    // if(source == NULL) {
-    //     fprintf(stderr, "Failed to open source file: %s\n", strerror(errno));
-    //     exit(1);
-    // }
-    // destination = fopen(".backup/", "w");
-    // if(destination == NULL) {
-    //     fprintf(stderr, "Failed to create backup file: %s\n", strerror(errno));
-    //     exit(1);
-    // }
+    strcpy(backupFile, "");
+    strcat(backupFile, ".backup/");
+    strcat(backupFile, dirEntry->d_name);
+    // printf("Source filename: %s\n", newNode->filename);
+    // printf("Source file: %s\n", newNode->filepath);
+    source = fopen(newNode->filepath, "r");
+    if(source == NULL) {
+        fprintf(stderr, "Failed to open source file: %s\n", strerror(errno));
+        exit(1);
+    }
+    destination = fopen(backupFile, "w");
+    if(destination == NULL) {
+        fprintf(stderr, "Failed to create backup file: %s\n", strerror(errno));
+        exit(1);
+    }
 
-    // if(pthread_mutex_lock(&fileLock) != 0) {
-    //     fprintf(stderr, "Failed to lock mutex: %s\n", strerror(errno));
-    //     exit(1);
-    // }
+    if(pthread_mutex_lock(&fileLock) != 0) {
+        fprintf(stderr, "Failed to lock mutex: %s\n", strerror(errno));
+        exit(1);
+    }
 
-    // while(1) {
-    //     //Copy file
+    while(1) {
+        //Copy file
+        printf("Copy File....\n");
+        break;
+    }
 
-    // }
-
-    // if(pthread_mutex_unlock(&fileLock) != 0) {
-    //     fprintf(stderr, "Failed to unlock mutex: %s\n", strerror(errno));
-    //     exit(1);
-    // }
-    // pthread_exit(NULL);
+    if(pthread_mutex_unlock(&fileLock) != 0) {
+        fprintf(stderr, "Failed to unlock mutex: %s\n", strerror(errno));
+        exit(1);
+    }
+    pthread_exit(NULL);
     
 }
 
@@ -54,7 +64,6 @@ void threadHandler() {
         fprintf(stderr, "Failed to create thread: %s\n", strerror(errno));
         exit(1);
     }
-    
     if(pthread_join(file[fileCounter], NULL) != 0) {
         fprintf(stderr, "Failed to join thread: %s\n", strerror(errno));
         exit(1);
@@ -68,8 +77,8 @@ If file is a regular file, call threadHandler() and increment fileCounter
 If file is a sub directory, recursively call countFiles()
 */
 int countFiles(char *cwd) {
-    char newPath[256];
     DIR *dir;
+    char newPath[256];
 
     dir = opendir(cwd);
     printf("cwd: %s\n", cwd);
@@ -82,6 +91,10 @@ int countFiles(char *cwd) {
         if(dirEntry->d_type == DT_REG) {
             printf("DT_REG: %s\n", dirEntry->d_name);
             sleep(1);
+            newNode->filename = dirEntry->d_name;
+            newNode->filepath = cwd;
+            // printf("newNode->filename: %s\n", newNode->filename);
+            // printf("newNode->filepath: %s\n", newNode->filepath);
             threadHandler();
             fileCounter++;
         }
@@ -141,7 +154,7 @@ int main(int argc, char *argv[]) {
     char cwd[256];
     fileCounter = 0;
     dirEntry = NULL;
-
+    newNode = malloc(sizeof(struct node *));
     //Check for -r command
     if(argc > 1) {
         //Call function to restore all backup files from .backup/
@@ -158,6 +171,6 @@ int main(int argc, char *argv[]) {
     else {
         countFiles(cwd);
     }
-    
+
     return 0;
 }
